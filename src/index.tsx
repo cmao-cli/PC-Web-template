560eead20d218757c4d9998338769a86e467be6b
wangzhen <wangzhen@codemao.cn>
2019-10-25 11:36:18 +0800

    fix: add mlz-lint规范ts

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,19 @@ import * as React from 'react';
 import { Provider } from 'react-redux';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { create_store } from './redux/root_store';;
+import { create_store } from './redux/root_store';
 import { RootRouter } from './router';
 
 require('./commons/style.scss');
 const store = create_store();
 const root_element = document.getElementById('root');
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <RootRouter/>
-    </BrowserRouter>
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <RootRouter/>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
   root_element,
 );
