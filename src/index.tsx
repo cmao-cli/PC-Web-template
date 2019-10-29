4fc094dd6c5ba53bf54124c81ce30540c5d9c623
luoqian <szuluoqian@163.com>
2019-10-29 11:05:24 +0800

    chore:删除注释

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,7 @@
-// import 'react-hot-loader';
 import * as React from 'react';
-// import { Provider } from 'react-redux';
 import * as ReactDOM from '@hot-loader/react-dom';
 import { Provider } from 'react-redux';
-// import { BrowserRouter } from 'react-router-dom';
 import { create_store } from './redux/root_store';;
-// import { RootRouter } from './router';
 import App from './App';
 
 require('./commons/style.scss');
@@ -20,12 +16,4 @@ ReactDOM.render(
   root_element,
 );
 
-// if ((module as any).hot) {
-//   (module as any).hot.accept('./App.tsx', () => {
-//     ReactDOM.render(
-//       <App/>,
-//       root_element,
-//     );
-//   });
-// }
 
